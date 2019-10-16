ee3b68d6e046e5a5ea9964479a6f9f174b91bafe
Jesus Diaz <jesus.diaz@cloud39.co>
2019-10-16 10:51:13 -0400

    Updating main

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -4,8 +4,3 @@ provider "google" {
 
   # access_token = "${data.vault_generic_secret.gcp_auth_token.data["token"]}"
 }
-
-resource "google_storage_bucket" "datalake-bucket" {
-    name     = "${var.data_project}-datalake"
-    project = "${var.data_project}"
-}
