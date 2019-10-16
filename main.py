00e266870b055bd75c0a31b754e210f8bdd8258d
Ariel Gonzalez <arieljosegcloud39@bitbucket.org>
2019-10-16 13:07:17 -0400

    rollback main.py

--- main.py
+++ main.py
@@ -12,139 +12,43 @@ db_config = {
     'password': 'PnNZ)58}&k=?jybfpYpi4@TIfB@V{9'
 }
 
-db_schemas =[
-                { 'database': 'billing',
-                  'tables': [
-                     {'name': 'billing_method',
-                      'schema': (
-                         'id:INTEGER, billing_profile_id:INTEGER, workspace_id:INTEGER, type:STRING, user_id:STRING, date_created:TIMESTAMP,'
-                         'date_modified:TIMESTAMP, data:STRING, metadata:STRING, team_id:STRING, inactive:BOOL,'
-                         'limit:INTEGER, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                     {'name': 'billing_profile',
-                      'schema': (
-                         'id:INTEGER, user_id:STRING, workspace_id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP,'
-                         'metadata:STRING, default_billing_method_id:INTEGER, default_payout_method_id:INTEGER, team_id:STRING,'
-                         'etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                     {'name': 'coupon',
-                      'schema': (
-                         'id:INTEGER, code:STRING, workspace_id:INTEGER, friendly_name:STRING, description:STRING,'
-                         'max_total_redemptions:INTEGER, date_expires:TIMESTAMP, duration:STRING, coupon_type:STRING,'
-                         'referrer_user_id:STRING, created_by_user_id:STRING, off_type:STRING, percent_off:FLOAT,'
-                         'amount_off:FLOAT, date_created:TIMESTAMP, date_modified:TIMESTAMP, max_amount_off:FLOAT,'
-                         'max_percent_off:FLOAT, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                     {'name': 'user_coupon',
-                      'schema': (
-                         'id:INTEGER, coupon_id:INTEGER, user_id:STRING, redeemed:BOOL, date_redeemed:TIMESTAMP,'
-                         'date_created:TIMESTAMP, date_modified:TIMESTAMP, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                     {'name': 'credit_transactions',
-                      'schema': (
-                         'id:INTEGER, transaction_type:STRING, amount:INT64, billing_method_id:NUMERIC, version:INT64,'
-                          'date_created:TIMESTAMP, metadata:STRING, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                     {'name': 'payout_method',
-                      'schema': (
-                          'id:INTEGER, billing_profile_id:INTEGER, workspace_id:INTEGER, type:STRING, user_id:STRING, date_created:TIMESTAMP,'
-                          'date_modified:TIMESTAMP, data:STRING, metadata:STRING, team_id:STRING, etl_region:STRING,'
-                          'etl_date_updated:TIMESTAMP')},
-                     ]},
-                { 'database': 'documents',
-                  'tables': [
-                    {'name': 'document',
-                      'schema': (
-                          'id:INTEGER, title:STRING, description:STRING, owner_id:STRING, '
-                          'workspace_id:STRING, archived:BOOL, date_created:TIMESTAMP, date_modified:TIMESTAMP, '
-                          'etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                    {'name':'document_access',
-                     'schema': (
-                            'id:INTEGER, grantor_id:STRING,type:STRING, job_id:STRING, edit_permission:STRING,'
-                            'date_created:TIMESTAMP, date_modified:TIMESTAMP, document_id:INTEGER, etl_region:STRING,'
-                            'etl_date_updated:TIMESTAMP')},
-                    {'name': 'document_tag',
-                     'schema': (
-                            'id:INTEGER, document_id:INTEGER, tag_id:INTEGER, etl_region:STRING,'
-                            'etl_date_updated:TIMESTAMP')},
-                    {'name':'document_version',
-                     'schema':(
-                            'id:INTEGER, files:STRING, date_created:TIMESTAMP, author_id:STRING, message:STRING,'
-                            'document_id:INTEGER, purged:BOOL, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                    {'name':'tag',
-                     'schema':(
-                            'id:INTEGER, label:STRING, color:STRING, workspace_id:STRING, date_created:TIMESTAMP,'
-                            'date_modified:TIMESTAMP, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                    ]},
-                { 'database':'iam',
-                  'tables': [
-                  {'name':'device',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP,  date_modified:TIMESTAMP, user_id:STRING,'
-                          'enabled:BOOL, data:STRING, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'invitation',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, email:STRING,'
-                          'token:STRING, workspace_id:INTEGER, workspace_role_id:INTEGER, team_id:INTEGER,'
-                          'team_role_id:INTEGER, expires:TIMESTAMP, created_by_user_id:STRING, status:STRING,'
-                          'etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'role',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'name:STRING, description:STRING, is_owner_role:BOOL, permissions:STRING,'
-                          'etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'role_member',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, member_id:INTEGER,'
-                          'role_id:INTEGER, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'skill',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'name:STRING, description:STRING, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'skill_member',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, member_id:INTEGER,'
-                          'skill_id:INTEGER, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'team',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, slug:STRING, name:STRING,'
-                          'owner_id:STRING, group_chat_conversation_id:INTEGER, inactive:BOOL, workspace_id:INTEGER,'
-                          'picture:STRING, address:STRING, team_purpose:STRING, etl_region:STRING,'
-                          'etl_date_updated:TIMESTAMP')},
-                  {'name':'team_member',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'team_id:INTEGER, user_id:STRING, inactive:BOOL, team_role_id:INTEGER, etl_region:STRING,'
-                          'etl_date_updated:TIMESTAMP')},
-                  {'name':'team_role',
-                   'schema':(
-                          'id:INTEGER,  date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'name:STRING, is_owner_role:BOOL, description:STRING, permissions:STRING, etl_region:STRING,'
-                          'etl_date_updated:TIMESTAMP')},
-                  {'name':'workspace',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, slug:STRING, name:STRING,'
-                          'branding:STRING, config:STRING,  default_role_id:INTEGER, etl_region:STRING,'
-                          'etl_date_updated:TIMESTAMP')},
-                  {'name':'workspace_domain',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'domain:STRING, target:STRING, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                  {'name':'workspace_member',
-                   'schema':(
-                          'id:INTEGER, date_created:TIMESTAMP, date_modified:TIMESTAMP, workspace_id:INTEGER,'
-                          'user_id:STRING, given_name:STRING, family_name:STRING, email:STRING, picture:STRING,'
-                          'phone:STRING, inactive:BOOL, max_concurrent_provider_jobs:INTEGER, member_permissions:STRING,'
-                          'notification_preferences:STRING, approved_terms_version:STRING, referrer:STRING,'
-                          'referrer_metadata:STRING, member_metadata:STRING, workspace_metadata:STRING,'
-                          'last_login:TIMESTAMP, last_seen:TIMESTAMP, etl_region:STRING, etl_date_updated:TIMESTAMP')},
-                ]},
-                { 'database': 'jobs',
-                  'tables': [
-                     {'name': 'job',
-                      'schema': ()},
-                     {'name': 'job_event',
-                      'schema': ()},
-                     {'name': 'legend_cache',
-                      'schema': ()},
-                     {'name': 'legend_ref',
-                      'schema': ()},
-                     ]},
+db_schemas = [{'database': 'billing', 'tables': [{'name': 'billing_method', 'schema': (
+                    'id:INTEGER, billing_profile_id:INTEGER, workspace_id:INTEGER, type:STRING, user_id:STRING, date_created:TIMESTAMP,'
+                    'date_modified:TIMESTAMP, data:STRING, metadata:STRING, team_id:STRING, inactive:BOOL, '
+                    'limit:INTEGER, etl_region:STRING, etl_date_updated:TIMESTAMP')}]},
+                                                 # {'name': 'billing_profile', 'schema': (
+                                                 #     'id:NUMERIC, user_id:STRING, workspace_id:NUMERIC, date_created:DATETIME, date_modified:DATETIME,'
+                                                 #     'metadata:STRING, default_billing_method_id:NUMERIC, default_payout_method_id:NUMERIC, team_id:STRING,'
+                                                 #     'region:STRING, updated_at:DATETIME')},
+                                                 # {'name': 'coupon', 'schema': (
+                                                 #     'id:NUMERIC, code:STRING, workspace_id:NUMERIC, friendly_name:STRING, description:STRING,'
+                                                 #     'max_total_redemptions:NUMERIC, date_expires:DATETIME, duration:STRING, coupon_type:STRING,'
+                                                 #     'referrer_user_id:STRING, created_by_user_id:STRING, off_type:STRING, percent_off:FLOAT,'
+                                                 #     'amount_off:FLOAT, date_created:DATETIME, date_modified:DATETIME, max_amount_off:FLOAT, max_percent_off:FLOAT,'
+                                                 #     'region:STRING, updated_at:DATETIME')},
+                                                 # {'name': 'user_coupon', 'schema': (
+                                                 #     'id:NUMERIC, coupon_id:NUMERIC, user_id:STRING, redeemed:BOOL, date_redeemed:DATETIME,'
+                                                 #     'date_created:DATETIME, date_modified:DATETIME, region:STRING, updated_at:DATETIME')}]},
+
+              # {
+              #     'database': 'documents',
+              #     'tables': [{
+              #         'name': 'document',
+              #         'schema': (
+              #             'id:INTEGER, title:STRING, description:STRING, owner_id:STRING, '
+              #             'workspace_id:STRING, archived:BOOL, date_created:TIMESTAMP, date_modified:TIMESTAMP, '
+              #             'etl_region:STRING, etl_date_updated:TIMESTAMP')},
+              #                                       {'name':'document_access', 'schema': (
+              #               'id:INTEGER, grantor_id:STRING,type:STRING, job_id:STRING, edit_permission:STRING,'
+              #               'date_created:TIMESTAMP, date_modified:TIMESTAMP, document_id:INTEGER, etl_region:STRING,'
+              #               'etl_date_updated:TIMESTAMP')}
+              #     ]
+              # },
+                                                # 'document_tag', 'document_version',
+              #                                      'tag']},
+              # {'database': 'iam', 'tables': ['device', 'invitation', 'role', 'role_member', 'skill', 'skill_member',
+              #                                'team', 'team_member', 'team_role', 'workspace', 'workspace_domain',
+              #                                'workspace_membet']},
               # {'database': 'jobs', 'tables': ['job','job_event', 'legend_cache', 'legend_ref']},
               # {'database': 'legends', 'tables': ['legend', 'legend_version']},
               # {'database': 'messages', 'tables': ['conversation', 'conversation_participant', 'message',
@@ -164,7 +68,7 @@ def get_db_source_config(database):
         )
 
 
-def fix_data_type(element):
+def fix_dates(element):
 
     for key,value in element.items():
 
@@ -184,7 +88,7 @@ def run(argv=None):
     parser = argparse.ArgumentParser()
     # parser.add_argument('--input',
     #                     dest='input',
-    #                     default='gs://taxfyle-qa-data/data/raw/',
+    #                     default='gs://dataflow-samples/shakespeare/kinglear.txt',
     #                     help='Input file to process.')
     # parser.add_argument('--output',
     #                     dest='output',
@@ -198,7 +102,8 @@ def run(argv=None):
     pipeline_options.view_as(SetupOptions).save_main_session = True
 
     timestamp = datetime.datetime.now().isoformat()
-    gcp_bucket = 'gs://c39-txf-sandbox/raw' #'gs://taxfyle-qa-data/data/raw'
+    gcp_bucket = 'gs://c39-txf-sandbox/raw'
+                #'gs://taxfyle-qa-data/data/raw'
 
     with beam.Pipeline(options=PipelineOptions()) as p:
 
@@ -219,14 +124,14 @@ def run(argv=None):
             database=e['database'],
         ), table_name=e['table']['name']) \
                     | "Pre-processing pipeline records for: {}[{}]".format(d['database'], e['table']['name']) \
-                    >> beam.Map(fix_data_type)
+                    >> beam.Map(fix_dates)
 
                 records | "Writing records to raw storage for: {}[{}]".format(d['database'], e['table']['name']) \
                     >> beam.io.WriteToText('{}/{}/{}/{}.jsonl'.format(gcp_bucket, e['database'], timestamp, e['table']['name']))
 
                 records | "Writing records to BQ for: {}[{}]".format(d['database'], e['table']['name']) \
                     >> beam.io.WriteToBigQuery(
-                        'cloud39-sandbox:c39_txf_sandbox.{}'.format(e['table']['name']),
+                         'cloud39-sandbox:c39_txf_sandbox.{}'.format(e['table']['name']),
                         #'taxfyle-qa-data:txf_dwh.{}'.format(e['table']['name']),
                           schema=e['table']['schema'],
                          # Creates the table in BigQuery if it does not yet exist.
