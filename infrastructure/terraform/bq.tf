308b9c07c80cc73dc170e77a1251f14cdaf747cc
Roly Vicaria <roly.vicaria@taxfyle.com>
2019-11-20 13:42:41 -0500

    Fix dataset name to use underscores

--- infrastructure/terraform/bq.tf
+++ infrastructure/terraform/bq.tf
@@ -1,8 +1,8 @@
 # This is the BigQuery dataset where the main data warehouse will be hosted
 resource "google_bigquery_dataset" "dataset" {
   count         = length(var.regions)
-  dataset_id    = "${var.dest_dataset}-${lookup(var.regions[count.index], "name")}"
-  friendly_name = "${var.dest_dataset}-${lookup(var.regions[count.index], "name")}"
+  dataset_id    = "${var.dest_dataset}_${lookup(var.regions[count.index], "name")}"
+  friendly_name = "${var.dest_dataset}_${lookup(var.regions[count.index], "name")}"
   description   = "Taxfyle data warehouse dataset - ${lookup(var.regions[count.index], "name")}"
   location      = lookup(var.regions[count.index], "bigquery_dataset_location")
 }
