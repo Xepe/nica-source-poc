d62975ec418c74c20d1cb5060423d21dcfb4a1a4
Roly Vicaria <roly.vicaria@taxfyle.com>
2019-11-21 10:07:14 -0500

    Fix trigger function gcs file name and app engine count

--- infrastructure/terraform/app_engine.tf
+++ infrastructure/terraform/app_engine.tf
@@ -1,5 +1,4 @@
 resource "google_app_engine_application" "app" {
-  count       = length(var.regions)
   project     = var.data_project
-  location_id = lookup(var.regions[count.index], "region")
+  location_id = "us-central1"
 }
--- infrastructure/terraform/storage.tf
+++ infrastructure/terraform/storage.tf
@@ -13,10 +13,10 @@ resource "google_storage_bucket" "code-bucket" {
   project       = var.data_project
   location      = "us-central1"
   force_destroy = true
+}
 
-  # provisioner "local-exec" {
-  #   command = "python ./../../code/pipeline_df/main-df.py --runner DataflowRunner --project ${var.data_project} --service_account_email ${google_service_account.service-project-service-account-data-pipeline.email} --temp_location gs://${google_storage_bucket.datalake-bucket.name}/tmp --staging_location gs://${google_storage_bucket.code-bucket.name}/binaries --subnetwork https://www.googleapis.com/compute/v1/projects/${var.host_project}/regions/${var.network_region}/subnetworks/${var.host_project_sub_network} --region ${var.network_region} --requirements_file ./../../code/pipeline_df/requirements.txt --template_location gs://${google_storage_bucket.code-bucket.name}/templates/pipeline-template-${lower(lookup(var.regions[count.index], "name"))} --db_host ${var.db_host} --db_port ${var.db_port} --db_user ${var.db_user} --db_password '${var.db_password}' --dest_dataset ${var.dest_dataset} --dest_bucket ${google_storage_bucket.datalake-bucket.name} --etl_region ${lookup(var.regions[count.index], "name")}"
-  # }
+locals {
+    trigger_function_gcs_filename = "trigger_function_${substr(lower(replace(base64encode(data.archive_file.trigger-function.output_md5), "=", "")), 0, 15)}.zip"
 }
 
 # Zip Python trigger_function folder
@@ -28,8 +28,7 @@ data "archive_file" "trigger-function" {
 
 # Provisioning trigger to bucket
 resource "google_storage_bucket_object" "trigger-function-zip" {
-  name       = "trigger_function.zip"
-  source     = ".${replace(path.module, path.root, "")}/code/trigger_function.zip"
+  name       = local.trigger_function_gcs_filename
+  source     = data.archive_file.trigger-function.output_path
   bucket     = google_storage_bucket.code-bucket.name
-  depends_on = [google_storage_bucket.code-bucket]
 }
